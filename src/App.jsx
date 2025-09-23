0f3537f17120d1e42ed06b3c23b5a5dce38304c5
ttop-stack <ttopacosta7@gmail.com>
2025-09-23 15:10:37 +0000

    migrating home page and importing it using apps.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import Sell from "./pages/Sell";
 import Archive from "./pages/Archive";
 import Contact from "./pages/Contact";
