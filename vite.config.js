f5716c5ac6b1fb88a7e5ab6f8c3ca820ac1a97ed
ttop-stack <ttopacosta7@gmail.com>
2025-09-23 15:15:57 +0000

    optimize for cloudflare

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './src',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
   },
+  publicDir: 'public',
 });
